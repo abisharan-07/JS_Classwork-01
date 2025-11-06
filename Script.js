13786f2bc206c0c8b08c04e25eebce80d09583b4
“abisharan-07” <kunam.abisharan07@gmail.com>
2025-11-06 12:36:41 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,22 @@
+document.getElementById("text").innerHTML="Welcome to JavaScript";
+document.write("This is my first line");
+window.alert("Hello, welcome to my website!");
+console.log("JavaScript is Fun!");
+
+const name="Sharan";
+let age=20;
+var F_Subject="Maths";
+    console.log("Name :",name);
+    console.log("Age :",age)
+    console.log("Favorite subject :",F_Subject)
+
+let F_Color=prompt("Input your favorite color :");
+    document.body.style.backgroundColor=F_Color;
+
+let quantity=prompt("Enter quantity :");
+const price=200;
+let total=quantity*price;
+    console.log("Total :",total)
+
+
+
